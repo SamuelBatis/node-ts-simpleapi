f06beb37d7f13db3358d47271bd9ca83f8c77fc3
Samuel Batistela <samuel.batistela12@gmail.com>
2024-03-14 17:27:07 -0300

    feat: add CreateUsersController with validations

--- src/controllers/create-user/create-user.ts
+++ src/controllers/create-user/create-user.ts
@@ -1,3 +1,4 @@
+import validator from "validator";
 import { User } from "../../models/user";
 import { HttpRequest, HttpResponse } from "../protocols";
 import {
@@ -12,13 +13,26 @@ export class CreateUserController implements ICreateUserController {
     httpRequest: HttpRequest<CreateUserParams>,
   ): Promise<HttpResponse<User>> {
     try {
-      if (!httpRequest.body) {
+      const requiredFields = ["firstName", "lastName", "email", "password"];
+
+      for (const field of requiredFields) {
+        if (!httpRequest?.body?.[field as keyof CreateUserParams]?.length) {
+          return {
+            statusCode: 400,
+            body: `Field ${field} id required`,
+          };
+        }
+      }
+      const emailIsValid = validator.isEmail(httpRequest.body!.email);
+      if (!emailIsValid) {
         return {
           statusCode: 400,
-          body: "Please specify a body",
+          body: "Email is invalid",
         };
       }
-      const user = await this.createUserRepository.createUser(httpRequest.body);
+      const user = await this.createUserRepository.createUser(
+        httpRequest.body!,
+      );
       return {
         statusCode: 201,
         body: user,
