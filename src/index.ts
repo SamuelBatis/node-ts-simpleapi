c417ca4798415a0bec60514b70b61d7d9b742965
Samuel Batistela <samuel.batistela12@gmail.com>
2024-03-14 18:08:02 -0300

    feat: add UpdateUserController with validations

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-user
 import { MongoClient } from "./database/mongo";
 import { MongoCreateUserRepository } from "./repositories/create-users/mongo-create-users";
 import { CreateUserController } from "./controllers/create-user/create-user";
+import { MongoUpdateUserRepository } from "./repositories/update-user/mongo-update-user";
+import { UpdateUserController } from "./controllers/update-user/update-user";
 
 config();
 
@@ -34,6 +36,22 @@ const main = async () => {
     });
     res.status(statusCode).send(body);
   });
+
+  app.patch("/users/:id", async (req, res) => {
+    const mongoUpdateUserRepository = new MongoUpdateUserRepository();
+
+    const updateUserController = new UpdateUserController(
+      mongoUpdateUserRepository,
+    );
+
+    const { body, statusCode } = await updateUserController.handle({
+      body: req.body,
+      params: req.params,
+    });
+
+    res.status(statusCode).send(body);
+  });
+
   const port = process.env.PORT || 8000;
 
   app.listen(port, () => console.log(`listening on port ${port}`));
